c012d748df69826584b3e202328bf76023a0cc79
monisha-gowda97 <monishagowda@ymail.com>
2020-09-08 21:01:29 -0400

    completed logbase2 program

--- BitMagic/src/LogBase2.java
+++ BitMagic/src/LogBase2.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class LogBase2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(findLog(scanner.nextInt()));
+    }
+
+    private static int findLog(int number){
+        int answer=0;
+        // right shift by 1 till number becomes 0
+        while((number>>=1)!=0){
+            // count number of shifts
+            answer++;
+        }
+        return answer;
+    }
 }
